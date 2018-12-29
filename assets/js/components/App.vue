dc0b1a9a0c310f47bad3d16ae9aca7847dfecdb3
Denys <d.bezuhlyi@devatlant.com>
2018-12-29 12:52:36 +0200

    Added vue js and bootstrap

---
+++ assets/js/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+    </div>
+</template>
+
+<script>
+    import Header from './Header';
+    export default {
+        components: {
+            appHeader: Header
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var Encore = require('@symfony/webpack-encore');
+const { VueLoaderPlugin } = require('vue-loader');
+
+Encore
+    .setOutputPath('public/build')
+
+    .setPublicPath('/build')
+
+    .addEntry('app', './assets/js/app.js')
+
+    .cleanupOutputBeforeBuild()
+
+    .enableBuildNotifications()
+
+    .enableVueLoader();
+
+module.exports = Encore.getWebpackConfig();
